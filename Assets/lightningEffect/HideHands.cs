875a937f1f30d3f4925edbc6520796611c2aa74e
Tasuku <lightfox.task@gmail.com>
2019-01-10 02:46:02 +0900

    Modified calibration for 1-10,inc's project north star.

---
+++ Assets/lightningEffect/HideHands.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject leftHand;        
+    GameObject rightHand;        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (this.leftHand == null) { this.leftHand = GameObject.Find('Left Interaction Hand Contact Bones'); }
+        if (this.rightHand == null) { this.rightHand = GameObject.Find('Right Interaction Hand Contact Bones'); }
+        if (this.leftHand.activeSelf) this.leftHand.SetActive(false);
+        if (this.rightHand.activeSelf) this.leftHand.SetActive(false);
+        */
+    }
+}
