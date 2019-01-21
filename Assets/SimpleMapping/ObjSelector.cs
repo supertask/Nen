243128f43732f646c9dadde6dfb5a00c19faa286
Tasuku <lightfox.task@gmail.com>
2019-01-22 00:30:44 +0900

    Added simple evironment mapping module.

---
+++ Assets/SimpleMapping/ObjSelector.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap;
+using Leap.Unity;
+
+public class ObjSelector : MonoBehaviour
+{
+    public GameObject leapProviderObj;
+    LeapServiceProvider m_Provider;
+    private LayerMask FIELD_LAYER;
+    public static float CASTING_SIZE = 0.05f; //diameter = 0.1m
+
+    private GameObject selecting;
+
+    void Start() {
+        this.FIELD_LAYER = LayerMask.GetMask("Field");
+        this.m_Provider = this.leapProviderObj.GetComponent<LeapServiceProvider>();
+        this.selecting = GameObject.Find("Selecting");
+
+    }
+
+    void Update()
+    {
+        Frame frame = this.m_Provider.CurrentFrame;
+        foreach(Hand hand in frame.Hands)
+        {
+            Vector3 src = this.GetVector3(hand.PalmPosition);
+            Vector3 target =this.GetVector3(hand.PalmNormal);
+            Debug.DrawLine(src, target * 3, Color.red);
+            RaycastHit[] hitObjects = Physics.SphereCastAll(src, ObjSelector.CASTING_SIZE,
+                target, Mathf.Infinity, FIELD_LAYER);
+            this.SelectObjects(hitObjects);
+        }
+
+
+        //Hand leftHand = frame.Hands[0].IsLeft ? frame.Hands[0] : frame.Hands[1];
+        //Hand rightHand = frame.Hands[1].IsRight ? frame.Hands[1] : frame.Hands[0];
+
+        //var isHit = Physics.Raycast(transform.position, transform.forward, out hit, 100);
+
+    }
+
+    private void SelectObjects(RaycastHit[] hitObjs)
+    {
+        foreach (RaycastHit obj in hitObjs)
+        {
+            obj.transform.parent = this.selecting.transform;
+        }
+    }
+
+
+    private Vector3 GetVector3(Vector v)
+    {
+        return new Vector3(v.x, v.y, v.z);
+    }
+
+}
---
+++ Assets/SimpleMapping/SimpleMapper.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMapper : MonoBehaviour
+{
+    private GameObject originBox;
+    private GameObject mapped;
+    private GameObject selecting;
+    private GameObject field;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.originBox = GameObject.Find("OriginCube");
+        this.mapped = GameObject.Find("Mapped");
+        this.selecting = GameObject.Find("Selecting");
+        this.field = GameObject.Find("Field");
+        this.Load();
+    }
+
+    /*
+     * セーブデータを読み込む
+     */
+    void Load()
+    {
+        if (PlayerPrefs.HasKey("mappedPosList"))
+        {
+            Vector3[] posList = PlayerPrefsX.GetVector3Array("mappedPosList");
+            Quaternion[] rotList = PlayerPrefsX.GetQuaternionArray("mappedRotList");
+            Vector3[] scaleList = PlayerPrefsX.GetVector3Array("mappedScaleList");
+            for(int i = 0; i < posList.Length; i++)
+            {
+                GameObject obj = Object.Instantiate(this.originBox) as GameObject;
+                obj.transform.position = posList[i];
+                obj.transform.rotation = rotList[i];
+                obj.transform.localScale = scaleList[i];
+                obj.transform.parent = this.mapped.transform;
+                obj.GetComponent<Renderer>().enabled = true;
+            }
+        }
+        else {
+            //セーブデータがない場合，何もしない
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.B)) {
+            this.mapSelectedObjects(); //選択したオブジェクトをマッピングする
+
+            //boxを作成
+            Vector3 p = this.originBox.transform.position;
+            GameObject box = Object.Instantiate(originBox) as GameObject; //コピー
+            box.transform.position = p;
+            box.GetComponent<Renderer>().enabled = true;
+            box.transform.parent = this.selecting.transform;
+
+            //
+        } else if (Input.GetKeyUp(KeyCode.E)) {
+            //マッピング終了
+            this.mapSelectedObjects(); //選択したオブジェクトをマッピングする
+            this.Save();
+        }
+    }
+
+    /*
+     * 選択したオブジェクトをマップ済みオブジェクトへ登録する
+     */
+    private void mapSelectedObjects() {
+        foreach(Transform child in this.selecting.transform) {
+            child.parent = this.mapped.transform;
+        }
+    }
+
+    /*
+     * データをPlayerPrefsXに保存する
+     */
+    public void Save()
+    {
+        List<Vector3> savingPosList = new List<Vector3>() { };
+        List<Quaternion> savingRotList = new List<Quaternion>() { };
+        List<Vector3> savingScaleList = new List<Vector3>() { };
+        foreach(Transform transform in this.mapped.transform) {
+            savingPosList.Add(transform.position);
+            savingRotList.Add(transform.rotation);
+            savingScaleList.Add(transform.localScale);
+        }
+        PlayerPrefsX.SetVector3Array("mappedPosList", savingPosList.ToArray());
+        PlayerPrefsX.SetQuaternionArray("mappedRotList", savingRotList.ToArray());
+        PlayerPrefsX.SetVector3Array("mappedScaleList", savingScaleList.ToArray());
+    }
+
+    void OnApplicationQuit()
+    {
+    }
+}
